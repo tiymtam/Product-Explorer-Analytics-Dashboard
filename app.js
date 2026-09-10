fae2fd9c7cc0e16e568807947f5dfedbd59cc41f
Shaffa A <email-github-kamu@example.com>
2026-09-10 21:39:26 +0700

    Bagian 2

--- app.js
+++ app.js
@@ -23,3 +23,25 @@ function applyDiscounts(cartData) {
 }
 
 console.log("Hasil Diskon:", applyDiscounts(cart));
+
+// 2
+let products = [
+    { id: 1, title: "Laptop", price: 1200, category: "laptops", stock: 5 },
+    { id: 2, title: "Smartphone", price: 800, category: "phones", stock: 15 },
+    { id: 3, title: "Headphones", price: 100, category: "audio", stock: 3 }
+];
+
+// 2.1
+const findProductById = (productsData, id) => productsData.find(p => p.id === id);
+
+// 2.2
+const lowStockProducts = products.filter(p => p.stock < 10);
+
+// 2.3:
+function updateStock(productsData, id, newStock) {
+    return productsData.map(p => 
+        p.id === id ? { ...p, stock: newStock } : p
+    );
+}
+
+console.log("Bagian 2 - Produk Stok Sedikit:", lowStockProducts);
