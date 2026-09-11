fb84aef11b11dffced870890d335733e13066e6c
Shaffa A <email-github-kamu@example.com>
2026-09-11 13:12:41 +0700

    Bagian 7

--- app.js
+++ app.js
@@ -185,3 +185,34 @@ function linearSearchById(productsData, targetId) {
     return -1;
 }
 console.log("6.2 Linear Search ID 2 (Index ke-):", linearSearchById(products, 2));
+
+// 7
+// 7.1
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) return mid;
+        if (arr[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
+}
+console.log("7.1 Binary Search (Cari 7):", binarySearch([1, 3, 5, 7, 9], 7));
+
+// 7.2
+const sortedProductsByPrice = [...products].sort((a, b) => a.price - b.price);
+
+function binarySearchByPrice(sortedProducts, targetPrice) {
+    let left = 0;
+    let right = sortedProducts.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (sortedProducts[mid].price === targetPrice) return mid;
+        if (sortedProducts[mid].price < targetPrice) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
+}
+console.log("7.2 Binary Search Harga 1200 (Index ke-):", binarySearchByPrice(sortedProductsByPrice, 1200));
