58b471b23e7bd8e9452ffa4c20379597107678d3
Shaffa A <email-github-kamu@example.com>
2026-09-12 14:23:06 +0700

    Bagian 13

--- app.js
+++ app.js
@@ -332,3 +332,30 @@ function buildProductLookup(productsData) {
 }
 const productLookup = buildProductLookup(products);
 console.log("12.1 Product Lookup Get ID 2:", productLookup.get(2));
+
+// 13
+// 13.1
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+    push(item) {
+        this.items.push(item);
+    }
+    pop() {
+        return this.items.pop();
+    }
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+    isEmpty() {
+        return this.items.length === 0;
+    }
+}
+
+// 13.2
+const searchHistory = new Stack();
+searchHistory.push("laptop");
+searchHistory.push("phone");
+searchHistory.push("tablet");
+console.log("13.2 Undo Search:", searchHistory.pop(), "| Current History:", searchHistory.items);
